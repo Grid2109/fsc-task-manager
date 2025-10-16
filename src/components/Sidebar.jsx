a65b24d735b4864acbe79a5c17d884afdb58ffaf
Ingrid Lira <gridferreiradoa2109@gmail.com>
2025-10-16 18:52:21 -0300

    test: test

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,21 +4,21 @@ import TaskIcon from '../assets/icons/tasks.svg?react'
 
 export const Sidebar = () => {
   return (
-    <div className="h-screen w-64 bg-white">
-      <div className="space-y-4 px-8 py-6">
-        <h1 className="text-xl font-semibold text-[#00ADB5]">Task Manager</h1>
+    <div className='h-screen w-64 bg-white'>
+      <div className='space-y-4 px-8 py-6'>
+        <h1 className='text-xl font-semibold text-[#00ADB5]'>Task Manager</h1>
         <p>
           Um simples{' '}
-          <span className="text-[#00ADB5]">Organizador de tarefas.</span>
+          <span className='text-[#00ADB5]'>Organizador de tarefas.</span>
         </p>
       </div>
 
-      <div className="flex flex-col gap-2 p-2">
-        <SidebarButton variant="unselected">
+      <div className='flex flex-col gap-2 p-2'>
+        <SidebarButton variant='unselected'>
           <HomeIcon />
           Início
         </SidebarButton>
-        <SidebarButton variant="selected">
+        <SidebarButton variant='selected'>
           <TaskIcon />
           Minhas tarefas
         </SidebarButton>
