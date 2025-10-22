1e96521f261f088fd63bcd01ad503d9bdcbb4cfc
Ingrid Lira <gridferreiradoa2109@gmail.com>
2025-10-22 17:27:16 -0300

    feat: ensure tasks are listed on Tasks component

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { Button } from './Button'
 import AddIcon from '../assets/icons/add.svg?react'
 import TrashIcon from '../assets/icons/trash.svg?react'
@@ -5,8 +6,15 @@ import SunIcon from '../assets/icons/sun.svg?react'
 import CloudIcon from '../assets/icons/cloud-sun.svg?react'
 import MoonIcon from '../assets/icons/moon.svg?react'
 import { TasksSeparator } from './TasksSeparator'
+import { TASKS } from '../constants/tasks'
+import { TaskItem } from './TaskItem'
 
 export function Tasks() {
+  const [tasks] = useState(TASKS)
+  const morningTasks = tasks.filter((task) => task.time === 'morning')
+  const afternoonTasks = tasks.filter((task) => task.time === 'afternoon')
+  const eveningTasks = tasks.filter((task) => task.time === 'evening')
+
   return (
     <div className='w-full px-8 py-16'>
       <div className='flex w-full justify-between'>
@@ -31,12 +39,21 @@ export function Tasks() {
       <div className='rounded-xl bg-white p-6'>
         <div className='space-y-3'>
           <TasksSeparator icon={<SunIcon />} title='Manhã' />
+          {morningTasks.map((task) => (
+            <TaskItem key={task.id} task={task} />
+          ))}
         </div>
         <div className='my-6 space-y-3'>
           <TasksSeparator icon={<CloudIcon />} title='Tarde' />
+          {afternoonTasks.map((task) => (
+            <TaskItem key={task.id} task={task} />
+          ))}
         </div>
         <div className='space-y-3'>
           <TasksSeparator icon={<MoonIcon />} title='Noite' />
+          {eveningTasks.map((task) => (
+            <TaskItem key={task.id} task={task} />
+          ))}
         </div>
       </div>
     </div>
---
+++ src/constants/tasks.js
@@ -0,0 +1,44 @@
+export const TASKS = [
+  {
+    id: 1,
+    title: 'Estudar React',
+    description: 'Completar o curso de React na plataforma FSC',
+    time: 'morning',
+    status: 'done',
+  },
+  {
+    id: 2,
+    title: 'Estudar JavaScript',
+    description: 'Completar o curso de JavaScript na plataforma FSC',
+    time: 'morning',
+    status: 'done',
+  },
+  {
+    id: 3,
+    title: 'Treinar',
+    description: 'Fazer um treino de 30 minutos',
+    time: 'afternoon',
+    status: 'in_progress',
+  },
+  {
+    id: 4,
+    title: 'Fazer compras',
+    description: 'Comprar mantimentos para a semana',
+    time: 'afternoon',
+    status: 'in_progress',
+  },
+  {
+    id: 5,
+    title: 'Descansar',
+    description: 'Assistir a um filme ou série',
+    time: 'evening',
+    status: 'not_started',
+  },
+  {
+    id: 6,
+    title: 'Ler livro sobre finanças',
+    description: 'Ler o primeiro capítulo',
+    time: 'evening',
+    status: 'not_started',
+  },
+]
