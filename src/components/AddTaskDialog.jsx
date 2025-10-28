4c6b8495152d1d21c0447238f3b9067e59109909
Ingrid Lira <gridferreiradoa2109@gmail.com>
2025-10-28 17:57:36 -0300

    feat: ensure AddTaskDialog can be closed

--- src/components/AddTaskDialog.jsx
+++ src/components/AddTaskDialog.jsx
@@ -1,5 +1,8 @@
 import { createPortal } from 'react-dom'
-export const AddTaskDialog = ({ isOpen }) => {
+
+import { Button } from './Button'
+import { Input } from './Input'
+export const AddTaskDialog = ({ isOpen, handleClose }) => {
   if (!isOpen) return null
   return createPortal(
     <div className='fixed bottom-0 left-0 top-0 flex h-screen w-screen items-center justify-center backdrop-blur-md'>
@@ -8,6 +11,28 @@ export const AddTaskDialog = ({ isOpen }) => {
         <p className='mt-1 text-sm text-[#9A9C9F]'>
           Insira as informações abaixo
         </p>
+        <div className='flex w-[336px] flex-col space-y-4'>
+          <Input id='title' label='Título' placeholder='título da tarefa' />
+          <Input id='time' label='Horário' placeholder='horário' />
+          <Input
+            id='description'
+            label='Descrição'
+            placeholder='Descreva a tarefa'
+          />
+          <div className='flex gap-3'>
+            <Button
+              size='large'
+              variant='secondary'
+              className='w-full'
+              onClick={() => handleClose()}
+            >
+              Cancelar
+            </Button>
+            <Button size='large' className='w-full'>
+              Salvar
+            </Button>
+          </div>
+        </div>
       </div>
     </div>,
     document.body
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -76,7 +76,10 @@ export function Tasks() {
             <AddIcon />
           </Button>
 
-          <AddTaskDialog isOpen={addTaskDialogIsOpen} />
+          <AddTaskDialog
+            isOpen={addTaskDialogIsOpen}
+            handleClose={() => setAddTaskDialogIsOpen(false)}
+          />
         </div>
       </div>
       <div className='rounded-xl bg-white p-6'>
