7ad42aae7a8faf710aae6957502aed9a67588183
Ingrid Lira <gridferreiradoa2109@gmail.com>
2026-01-05 13:20:09 -0300

    feat: add InputLabel component

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,11 +1,10 @@
+import { InputLabel } from './InputLabel'
+
 export const Input = ({ label, ...rest }) => {
   return (
     <div className='flex flex-col space-y-1 text-left'>
-      <label htmlFor={rest.id} className='text-sm font-semibold text-[#35383E]'>
-        {label}
-      </label>
+      <InputLabel htmlFor={rest.id}>{label}</InputLabel>
       <input
-        type='text'
         className='rounded-lg border border-solid border-[#ECECEC] px-4 py-3 outline-[#00ADB5] placeholder:text-sm placeholder:text-[#9A9C9F]'
         {...rest}
       />
---
+++ src/components/InputLabel.jsx
@@ -0,0 +1,7 @@
+export const InputLabel = (props) => {
+  return (
+    <label {...props} className='text-sm font-semibold text-[#35383E]'>
+      {props.children}
+    </label>
+  )
+}
