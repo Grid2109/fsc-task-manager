571c7bb963b2c5c45c36f1ae1223d8d626a3367d
Ingrid Lira <gridferreiradoa2109@gmail.com>
2026-07-12 17:32:05 -0300

    refactor: add task query and mutation keys variables

--- src/hooks/data/use-add-task.jsx
+++ src/hooks/data/use-add-task.jsx
@@ -1,11 +1,12 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 
+import { taskMutationKeys } from '../../keys/mutations'
 import { api } from '../../lib/axios'
 
 export const useAddTask = () => {
   const queryClient = useQueryClient()
   return useMutation({
-    mutationKey: 'addTask',
+    mutationKey: taskMutationKeys.add(),
     mutationFn: async (task) => {
       const { data: createTask } = await api.post('/tasks', task)
       return createTask
