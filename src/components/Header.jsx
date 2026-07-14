c7901bc31a56b90c3bc82c41140bafad3109fe46
Ingrid Lira <gridferreiradoa2109@gmail.com>
2026-07-14 15:55:41 -0300

    feat: add dashboard cards

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,42 @@
 import PropTypes from 'prop-types'
+import { useState } from 'react'
 
-export function Header(props) {
-  return <header>{props.children}</header>
+import { AddIcon, TrashIcon } from '../assets/icons'
+import { AddTaskDialog } from './AddTaskDialog'
+import { Button } from './Button'
+
+export function Header({ subtitle, title }) {
+  const [addTaskDialogIsOpen, setAddTaskDialogIsOpen] = useState(false)
+
+  return (
+    <div className='flex w-full justify-between'>
+      <div>
+        <span className='text-xs font-semibold text-brand-primary'>
+          {subtitle}
+        </span>
+        <h2 className='text-xl font-semibold'>{title}</h2>
+      </div>
+
+      <div className='flex items-center gap-3'>
+        <Button color='ghost'>
+          Limpar tarefas
+          <TrashIcon />
+        </Button>
+        <Button color='primary' onClick={() => setAddTaskDialogIsOpen(true)}>
+          Nova tarefa
+          <AddIcon />
+        </Button>
+
+        <AddTaskDialog
+          isOpen={addTaskDialogIsOpen}
+          handleClose={() => setAddTaskDialogIsOpen(false)}
+        />
+      </div>
+    </div>
+  )
 }
 
 Header.propTypes = {
-  children: PropTypes.node.isRequired,
+  subtitle: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
 }
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,25 +1,15 @@
 import { useQueryClient } from '@tanstack/react-query'
-import { useState } from 'react'
 import { toast } from 'sonner'
 
-import {
-  AddIcon,
-  CloudIcon,
-  MoonIcon,
-  SunIcon,
-  TrashIcon,
-} from '../assets/icons'
+import { CloudIcon, MoonIcon, SunIcon } from '../assets/icons'
 import { useGetTasks } from '../hooks/data/use-get-tasks'
-import { AddTaskDialog } from './AddTaskDialog'
-import { Button } from './Button'
+import { Header } from './Header'
 import { TaskItem } from './TaskItem'
 import { TasksSeparator } from './TasksSeparator'
 
 export function Tasks() {
   const queryClient = useQueryClient()
   const { data: tasks } = useGetTasks()
-
-  const [addTaskDialogIsOpen, setAddTaskDialogIsOpen] = useState(false)
 
   const morningTasks = tasks?.filter((task) => task.time === 'morning')
   const afternoonTasks = tasks?.filter((task) => task.time === 'afternoon')
@@ -53,30 +43,7 @@ export function Tasks() {
 
   return (
     <div className='w-full space-y-6 px-8 py-16'>
-      <div className='flex w-full justify-between'>
-        <div>
-          <span className='text-xs font-semibold text-brand-primary'>
-            Minhas tarefas
-          </span>
-          <h2 className='text-xl font-semibold'>Minhas tarefas</h2>
-        </div>
-
-        <div className='flex items-center gap-3'>
-          <Button color='ghost'>
-            Limpar tarefas
-            <TrashIcon />
-          </Button>
-          <Button color='primary' onClick={() => setAddTaskDialogIsOpen(true)}>
-            Nova tarefa
-            <AddIcon />
-          </Button>
-
-          <AddTaskDialog
-            isOpen={addTaskDialogIsOpen}
-            handleClose={() => setAddTaskDialogIsOpen(false)}
-          />
-        </div>
-      </div>
+      <Header subtitle='Minhas tarefas' title='Minhas tarefas' />
       <div className='rounded-xl bg-white p-6'>
         <div className='space-y-3'>
           <TasksSeparator icon={<SunIcon />} title='Manhã' />
